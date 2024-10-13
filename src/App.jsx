574c9ffc1e8a625ce0867f9700beced1176ea429
gugrando <gustavo.vieira0909@gmail.com>
2024-10-13 01:58:37 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+<<<<<<< HEAD
 import Home from './pages/Home.jsx'
+=======
+import Home from './pages/Home'
+>>>>>>> 218a966 (.)
 
 
 function App() {
