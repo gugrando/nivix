5a98b9cd702406d40e69295089b3ed63cf96b6ec
gugrando <gustavo.vieira0909@gmail.com>
2024-10-13 02:23:29 -0300

    main1

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,11 +1,12 @@
-import Button from 'src/app/Utils/Button.jsx'
-
 function Home() {
     return (
-        <div>
-          <Button text='Nivix' />
-          <Button text='Nivix2' />
-        </div>
+        <>
+            <h1>TESTE</h1>
+            <h1>TESTE</h1>
+            <h1>TESTE</h1>
+            <h1>TESTE</h1>
+            <h1>TESTE</h1>
+        </>
     )
 }
 
