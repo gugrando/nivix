cb5cf92afcbda8a3bd69dcb691cdf4e51755cd32
gugrando <gustavo.vieira0909@gmail.com>
2024-10-13 01:05:41 -0300

    Estrutura Basica Criada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      
+      {/* Onde vai o quebra cabeça de páginas e o roteamento */}
     </>
   )
 }
