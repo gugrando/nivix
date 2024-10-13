841d6813028706d3bfe86ac1170de343e5862329
everthy1 <everthybonfim168@gmail.com>
2024-10-13 01:24:20 -0300

    criacao de rota2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from 'src\app\pages\Home.jsx'
+
 
 function App() {
 
   return (
     <>
-      {/* Onde vai o quebra cabeça de páginas e o roteamento */}
+      <Router> 
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
