655e18c19f388a35e327226c9e2aadb61800c097
gugrando <gustavo.vieira0909@gmail.com>
2024-10-13 02:06:08 -0300

    erro pkrl

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import './App.css'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-<<<<<<< HEAD
-import Home from './pages/Home.jsx'
-=======
 import Home from './pages/Home'
->>>>>>> 218a966 (.)
+
 
 
 function App() {
